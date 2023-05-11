4f178e081277420b5d1a5f72466d3926e79e2259
joafran0016 <joafran0016@gmail.com>
2023-05-11 18:50:42 -0300

    Adding react deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-
 import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [sitemap(), tailwind()]
+  integrations: [sitemap(), tailwind(), react()]
 });
