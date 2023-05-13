b473d03ab481df5f6cba6f5cdc5f222052fa1cc3
joafran0016 <joafran0016@gmail.com>
2023-05-13 16:50:47 -0300

    Adding post tags

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -8,10 +8,24 @@ export const defaultLang = "en";
 export const ui = {
   en: {
     "nav.source": "Source",
+    "home.hello": "Hey there! 👋",
+    "home.latest": "Latest posts",
+    "post.tags": {
+      experience: "Experience",
+      advice: "Advice",
+    },
+
     "footer.text": "All rights reserved",
   },
   es: {
     "nav.source": "Repo",
+    "home.hello": "Hola! 👋",
+    "home.latest": "Ultimas publicaciones",
+    "post.tags": {
+      experience: "Experiencia",
+      advice: "Consejo",
+    },
+
     "footer.text": "Todos los derechos reservados",
   },
 } as const;
