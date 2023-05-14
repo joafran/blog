8d3ff99a28b863ab0ac5854764ba32968f55d996
joafran0016 <joafran0016@gmail.com>
2023-05-14 17:25:58 -0300

    Adding space betwen tags

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -4,7 +4,7 @@ interface Props {
 
 const Tag: React.FC<Props> = ({ text }) => {
   return (
-    <div className="inline-flex bg-slate-800 text-white dark:bg-zinc-800 items-center leading-none px-2.5 py-1.5 text-sm font-medium text-skin-inverted rounded-full border border-skin-input">
+    <div className="inline-flex bg-slate-800 mr-2 text-white dark:bg-zinc-800 items-center leading-none px-2.5 py-1.5 text-sm font-medium text-skin-inverted rounded-full border border-skin-input">
       #{text}
     </div>
   );
